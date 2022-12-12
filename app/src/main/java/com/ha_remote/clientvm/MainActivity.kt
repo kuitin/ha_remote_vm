8af08e57c11806a09e553b297c4284ec8bb4ee96
kuitin <quentin00@hotmail.fr>
2022-12-12 11:23:31 +0100

    Binding both way worked

--- app/src/main/java/com/ha_remote/clientvm/MainActivity.kt
+++ app/src/main/java/com/ha_remote/clientvm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ha_remote.clientvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.ha_remote.clientvm.databinding.ActivityMainBinding
 import com.ha_remote.clientvm.ui.main.MainFragment
 import com.ha_remote.clientvm.ui.main.MainViewModel
@@ -15,12 +16,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activilty_main)
-        val homeBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val databinding = ViewModelProvider(this)[MainViewModel::class.java]
+        val homeBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding?>(this, R.layout.activity_main)
+            .apply { this.setLifecycleOwner ( this@MainActivity)
+            this.username = databinding}
         // For RecyclerView
 // For Text
-        val databinding = MainViewModel()
-        databinding.inputTitle = "DataBinding QDE"
-        homeBinding.username = databinding
+//        val databinding = MainViewModel()
+//        databinding.inputTitle.value = "DataBinding QDE"
+//        databinding.success.value = true
+//        homeBinding.username = databinding
+//        databinding.inputTitle.observe(owner:this, Observable{
+//            To
+//        })
 //        homeBinding.textName.text = "modified value QDE"
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/com/ha_remote/clientvm/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ha_remote/clientvm/ui/main/MainViewModel.kt
@@ -1,11 +1,33 @@
 package com.ha_remote.clientvm.ui.main
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class MainViewModel :  ViewModel(), Observable {
     // TODO: Implement the ViewModel
-    var inputTitle: String  = "defaultUndervalue"
-    var isSuccess: Boolean  = true
+    @Bindable
+    var inputTitle  = MutableLiveData<String> ()
 
+    @Bindable
+    var success = MutableLiveData<Boolean> ()
+
+    public fun onClick()
+    {
+        success.value = false
+        inputTitle.value = "button clicked"
+    }
+
+    private val callbacks: PropertyChangeRegistry by lazy { PropertyChangeRegistry()}
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        callbacks.add(callback)
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        callbacks.remove(callback)
+    }
 
 }
